ae7371af5ee92960fdbcb4947e00eef3ed9d1215
biantongfei <bian2016buaa@163.com>
2023-12-02 22:21:08 +0000

    gpu version

--- train_val.py
+++ train_val.py
@@ -1,14 +1,15 @@
-from Dataset import AvgDataset, get_data_path, get_tra_test_files
+from Dataset import AvgDataset, get_tra_test_files
 from Models import FCNN
 from draw_utils import draw_performance
 
 from torch.utils.data import DataLoader
-from torch import optim
-from torch.nn import MSELoss
+from torch import device, cuda, optim
+from torch.nn import MSELoss, functional
 import random
 
 batch_size = 128
 valset_rate = 0.1
+device = device("cuda:0" if cuda.is_available() else "cpu")
 
 
 def train_avg(action_recognition=True):
@@ -23,16 +24,16 @@ def train_avg(action_recognition=True):
                              is_crop=is_crop, is_coco=is_coco, dimension=dimension)
         net = FCNN(is_coco=is_coco, action_recognition=action_recognition)
         # net = CNN(is_coco=is_coco, action_recognition=action_recognition)
+        net.to(device)
         optimizer = optim.SGD(net.parameters())
         # optimizer = optim.adam(net.parameters())
         train_dict[key] = {'is_crop': is_crop, 'is_coco': is_coco, 'dimension': dimension, 'tra_files': tra_files,
-                           'testset': testset, 'test_loader': test_loader, 'net': net, 'optimizer': optimizer}
+                           'testset': testset, 'net': net, 'optimizer': optimizer}
 
     accuracy_dict = {'crop+coco': [], 'crop+halpe': [], 'noise+coco': [], 'noise+halpe': []}
     epoch = 0
     unimproved_epoches = 0
     while int(unimproved_epoches / len(train_dict.keys())) + 1 < 5:
-        continue_train = False
         for key in train_dict.keys():
             random.shuffle(train_dict['tra_files'])
             trainset = AvgDataset(data_files=train_dict['tra_files'][:int(len(tra_files) * valset_rate)],
@@ -43,30 +44,44 @@ def train_avg(action_recognition=True):
                                 is_crop=train_dict['is_crop'], is_coco=train_dict['is_coco'], dimension=dimension)
             train_loader = DataLoader(dataset=trainset, batch_size=batch_size)
             val_loader = DataLoader(dataset=valset, batch_size=batch_size)
+            net = train_dict['net']
+            optimizer = train_dict['optimizer']
             for idx, data in enumerate(train_loader):
                 inputs, labels = data
+                inputs, labels = inputs.to(device), labels.to(device)
                 outputs = net(inputs)
-                labels_onehot = one_hot(labels)
+                labels_onehot = functional.one_hot(labels)
                 loss = MSELoss(outputs, labels_onehot)
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
 
-        total_correct = 0
-        for idx, data in enumerate(val_loader):
-            inputs, labels = data
+            total_correct = 0
+            for idx, data in enumerate(val_loader):
+                inputs, labels = data
+                outputs = net(inputs)
+                inputs, labels = inputs.to(device), labels.to(device)
+                pred = outputs.argmax(dim=1)
+                correct = pred.eq(labels).sum().float().item()
+                total_correct += correct
+            acc = total_correct / len(val_loader.dataset)
+            accuracy_dict[key].append(acc)
+            if acc <= accuracy_dict[key][-2]:
+                unimproved_epoches += 1
+            else:
+                unimproved_epoches = 0
+            print('epcoch: %d, key: %s, acc: %s, unimproved_epoch: %d' % (
+                epoch, key, '{.2%f}' % (acc * 100), int(unimproved_epoches / len(train_dict.keys())) + 1))
+    for key in train_dict:
+        test_loader = DataLoader(dataset=train_dict[key]['testset'], batch_size=batch_size)
+        for idx, data in enumerate(test_loader):
+            inputs, labels = inputs.to(device), labels.to(device)
             outputs = net(inputs)
             pred = outputs.argmax(dim=1)
             correct = pred.eq(labels).sum().float().item()
             total_correct += correct
         acc = total_correct / len(val_loader.dataset)
-        accuracy_dict[key].append(acc)
-        if acc <= accuracy_dict[key][-2]:
-            unimproved_epoches += 1
-        else:
-            unimproved_epoches = 0
-        print('epcoch: %d, key: %s, acc: %s, unimproved_epoch: %d' % (
-            epoch, key, '{.2f%}' % (acc * 100), int(unimproved_epoches / len(train_dict.keys())) + 1))
+        print('key: %s, acc: %s' % (key, '{.2f}' % (acc * 100)))
     return train_dict
 
 
@@ -85,7 +100,7 @@ def cal_avg_performance(train_log):
 if __name__ == '__main__':
     train_log = []
     for i in range(1):
-        train_dict = train_avg(action_recognition == True)
+        train_dict = train_avg(action_recognition=True)
         train_log.append(train_dict)
     train_dict = cal_avg_performance(train_log)
     draw_performance(train_dict)
