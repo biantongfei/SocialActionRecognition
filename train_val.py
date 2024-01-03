c087031a5d5a3f4e46565bc4f40f96228e1bc49c
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-01-03 17:39:02 +0000

    just training for different params

--- train_val.py
+++ train_val.py
@@ -289,7 +289,7 @@ if __name__ == '__main__':
     performance = []
     for i in range(5):
         print('~~~~~~~~~~~~~~~~~~~%d~~~~~~~~~~~~~~~~~~~~' % i)
-        p = train_avg(action_recognition=2, body_part=[False, False, True], ori_videos=False, video_len=0)
+        p = train_avg(action_recognition=0, body_part=[True, False, False], ori_videos=False, video_len=0)
         performance.append(p)
     # traine_perframe(action_recognition=2, body_part=4)
     save_performance(performance)
