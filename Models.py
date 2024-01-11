ddce558172420f770bd112c0ff8cd3711b0181df
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-01-11 16:37:39 +0000

    perframe voting

--- Models.py
+++ Models.py
@@ -57,11 +57,11 @@ class DNN(nn.Module):
             self.fc = nn.Sequential(
                 nn.Linear(self.input_size, 128),
                 nn.ReLU(),
-                # nn.Dropout(0.5),
+                nn.Dropout(0.5),
                 nn.BatchNorm1d(128),
                 nn.Linear(128, 32),
                 nn.ReLU(),
-                # nn.Dropout(0.5),
+                nn.Dropout(0.5),
                 nn.BatchNorm1d(32),
                 nn.Linear(32, self.output_size),
                 nn.Softmax(dim=1)
