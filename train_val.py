790501f864f1c4d452a4847d691f8e101fa0f41f
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-01-13 19:29:07 +0000

    perframe voting

--- train_val.py
+++ train_val.py
@@ -222,8 +222,8 @@ if __name__ == '__main__':
     action_recognition = 1
     body_part = [True, False, True]
     ori_video = False
-    sample_fps = 6
-    video_len = 1
+    sample_fps = 3
+    video_len = 2
     performance_model = []
     for i in range(10):
         print('~~~~~~~~~~~~~~~~~~~%d~~~~~~~~~~~~~~~~~~~~' % i)
