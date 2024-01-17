c6934f10508543c8a6070fea412f3e54ebb121dd
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-01-17 02:36:05 +0000

    perframe voting

--- Models.py
+++ Models.py
@@ -42,7 +42,7 @@ class DNN(nn.Module):
             self.output_size = ori_action_class_num if action_recognition == 1 else action_class_num
         else:
             self.output_size = attitude_class_num
-        if model in ['avg', 'perframe']:
+        if model == 'avg':
             self.fc = nn.Sequential(
                 nn.Linear(self.input_size, 128),
                 nn.ReLU(),
