4813cef97c5b9f7ed6e7ef11d6923cb78edb18d4
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-02-02 12:38:13 +0000

    hierarchical classification task

--- train_val.py
+++ train_val.py
@@ -313,8 +313,8 @@ if __name__ == '__main__':
     model = 'perframe'
     body_part = [True, False, True]
     ori_video = False
-    sample_fps = 15
-    video_len = 1
+    sample_fps = 6
+    video_len = 2
     performance_model = []
     i = 0
     while i < 10:
