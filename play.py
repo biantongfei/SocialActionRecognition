e37529dd8f12d13a68d5069e3f5c000241efc1c3
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-02-29 14:57:09 +0000

    try git

--- play.py
+++ play.py
@@ -3,33 +3,21 @@ from torch import nn
 import numpy as np
 import math
 
-m1 = nn.Conv1d(266, 64, 7, stride=3, padding=3)
-m2 = nn.Conv1d(64, 32, 7, stride=3, padding=3)
-m3 = nn.Conv1d(32, 16, 7, stride=3, padding=3)
-input = torch.randn(32, 3, 266)
+m1 = nn.Conv1d(130, 128, 7, stride=3, padding=3)
+m2 = nn.Conv1d(128, 64, 5, stride=2, padding=2)
+m3 = nn.Conv1d(64, 32, 5, stride=2, padding=2)
+m4 = nn.Conv1d(32, 16, 3, stride=1, padding=1)
+m5 = nn.Conv1d(16, 8, 3, stride=1, padding=1)
+input = torch.randn(32, 30, 130)
 input = torch.transpose(input, 1, 2)
 output = m1(input)
 print(output.shape)
-output = nn.MaxPool1d(2, stride=2, padding=1)(output)
-print(output.shape)
 output = m2(output)
-print(output.shape)
-output = nn.MaxPool1d(2, stride=2, padding=1)(output)
 print(output.shape)
 output = m3(output)
 print(output.shape)
-output = nn.MaxPool1d(2, stride=2, padding=1)(output)
+output = m4(output)
+print(output.shape)
+output = m5(output)
 print(output.shape)
 print(int((math.ceil(int((math.ceil(int((math.ceil(3 / 3) + 2) / 2) / 3) + 2) / 2) / 3) + 2) / 2))
-180
-360
-1080
-
-1058
-529
-265
-60
-30
-15
-8
-3
