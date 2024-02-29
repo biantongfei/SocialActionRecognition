6b1c90f3d64b27b339cac7773d2985cdb8125ba6
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-02-29 21:25:32 +0000

    try git

--- train_val.py
+++ train_val.py
@@ -409,8 +409,8 @@ if __name__ == '__main__':
     # framework = 'attitude'
     # framework = 'action'
     # framework = 'parallel'
-    framework = 'tree'
-    # framework = 'chain'
+    # framework = 'tree'
+    framework = 'chain'
     ori_video = False
     sample_fps = 30
     video_len = 0.5
