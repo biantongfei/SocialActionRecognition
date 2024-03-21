941c326dd949ef8068b144d437c19f493ea71748
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-03-21 01:20:13 +0000

    GNN

--- Models.py
+++ Models.py
@@ -462,7 +462,7 @@ class GNN(torch.nn.Module):
             if self.model == 'gnn_keypoint_lstm':
                 on, _ = self.time_model(x_time)
                 on = on.reshape(on.shape[0], on.shape[1], 2, -1)
-                x = (torch.cat([on[:, -1, 0, :], on[:, 0, 1, :]], dim=0))
+                x = (torch.cat([on[:, -1, 0, :], on[:, 0, 1, :]], dim=-1))
                 print(x.shape)
                 if self.attention:
                     attention_weights = nn.Softmax(dim=1)(self.lstm_attention(x))
