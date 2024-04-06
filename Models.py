4b8e15cf51e8b0461d13a9656b618d94706c38de
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-04-06 19:51:21 +0800

    GNN

--- Models.py
+++ Models.py
@@ -373,7 +373,7 @@ class GNN(torch.nn.Module):
         time_edge_index = torch.tensor(np.array([[i, i + 1] for i in range(self.max_length - 1)]),
                                        dtype=torch.long).t().contiguous()
         if self.model != 'st-gcn':
-            x_time = torch.zeros((x.shape[0], x.shape[1], int(self.input_size / 2 * self.keypoint_hidden_dim))).to(
+            x_time = torch.zeros((x.shape[0], x.shape[1], int(self.input_size * self.keypoint_hidden_dim / 2))).to(
                 dtype).to(device)
             # x_time = torch.zeros((x.shape[0], x.shape[1], 69 * 4)).to(dtype).to(device)
             for i in range(x.shape[0]):
