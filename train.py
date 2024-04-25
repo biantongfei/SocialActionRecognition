ce4178dbe9e4eb8698d4aa5bdb5213061160deb7
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-04-25 19:40:28 +0100

    GNN

--- train.py
+++ train.py
@@ -12,7 +12,8 @@ framework = 'parallel'
 ori_video = False
 sample_fps = 30
 video_len = 2
-for model in ['avg', 'perframe', 'conv1d', 'lstm', 'gru']:
+# for model in ['avg', 'perframe', 'conv1d', 'lstm', 'gru']:
+for model in ['gcn_lstm', 'gcn_gcn']:
     performance_model = []
     i = 0
     while i < 1:
