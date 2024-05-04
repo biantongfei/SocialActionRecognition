fe14538a595c81890d30ec89dcc4e3332be68a7c
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-05 00:46:11 +0100

    GNN

--- Dataset.py
+++ Dataset.py
@@ -393,8 +393,7 @@ class Dataset(Dataset):
             edge_index_list += edge_index
             edge_attr_list += edge_attr
             frame_num += 1
-        print(len(edge_index_list))
-        for index in range(frame_num - 1):
+        for index in range(len(x_list) - int(input_size / 2)):
             edge_index_list.append([index, int(index + input_size / 2)])
         label = feature_json['intention_class'], feature_json['attitude_class'], feature_json['action_class']
         if len(x_list) == 0:
