d10879b5c4e41967ce84a2392e9e65fb9c508c32
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-05 01:48:45 +0100

    GNN

--- Models.py
+++ Models.py
@@ -422,7 +422,7 @@ class GNN(torch.nn.Module):
         else:
             x_batch = torch.zeros((x.shape[0], self.fc_input_size)).to(dtype).to(device)
             for i in range(x.shape[0]):
-                x_b = self.ST_GCN1(x=x[i], edge_index=edge_index[i], edge_attr=edge_attr[i])
+                x_b = self.ST_GCN1(x=x[i], edge_index=edge_index[i], edge_attr=edge_attr[i]).to(dtype).to(device)
                 x_b = x_b.flatten(1)
                 x_batch[i] = x_b
             x = x_batch
