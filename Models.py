4eb8e6b90258a5ade73706825bfdb9595daec6fe
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-05 01:33:45 +0100

    GNN

--- Models.py
+++ Models.py
@@ -388,6 +388,7 @@ class GNN(torch.nn.Module):
             for i in range(x.shape[0]):
                 for ii in range(x.shape[1]):
                     x_t, new_edge_index, edge_attr_t = x[i][ii], edge_index[i][ii], edge_attr[i][ii]
+                    print(x_t.shape, new_edge_index.shape, edge_attr_t.shape)
                     x_t = self.GCN_keypoints(x=x_t, edge_index=new_edge_index, edge_attr=edge_attr_t).to(dtype).to(
                         device)
                     # x_t, new_edge_index, _, _, _, _ = self.pool(x_t, new_edge_index)
@@ -420,7 +421,7 @@ class GNN(torch.nn.Module):
                     x[i] = x_t.reshape(1, -1)[0]
                 x = x.flatten(1)
         else:
-            print(x.shape,edge_index.shape,edge_attr.shape)
+            print(x.shape, edge_index.shape, edge_attr.shape)
             x = self.ST_GCN1(x=x, edge_index=edge_index, edge_attr=edge_attr)
             x = x.flatten(1)
         y = self.fc(x)
