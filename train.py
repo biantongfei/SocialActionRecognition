13d2eeca6fc5ea5263b9e40162dad166213e15be
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-05 03:07:41 +0100

    GNN

--- train.py
+++ train.py
@@ -33,4 +33,4 @@ for model in ['gcn_gru']:
     result_str = 'model: %s, body_part: [%s, %s, %s], framework: %s, sample_fps: %d, video_len: %s' % (
         model, body_part[0], body_part[1], body_part[2], framework, sample_fps, str(video_len))
     print(result_str)
-    # send_email(result_str)
+    send_email(result_str)
