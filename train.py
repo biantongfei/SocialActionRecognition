78bc386a154a74216c605486c2a9c611273c38bf
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-07 02:05:55 +0100

    ST-GNN

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ for model in ['stgcn']:
         #     continue
         performance_model.append(p_m)
         i += 1
-    draw_save('stgat', performance_model, framework)
+    draw_save('stgin', performance_model, framework)
     result_str = 'model: %s, body_part: [%s, %s, %s], framework: %s, sample_fps: %d, video_len: %s' % (
         model, body_part[0], body_part[1], body_part[2], framework, sample_fps, str(video_len))
     print(result_str)
