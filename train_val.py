9bbf35c2d1fac39ac81ecdf393e93246668d923b
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-14 19:34:57 +0100

    add confident score

--- train_val.py
+++ train_val.py
@@ -73,7 +73,9 @@ elif torch.backends.mps.is_available():
 else:
     print('Using CPU for training')
     device = torch.device('cpu')
+device = torch.device('cpu')
 dtype = torch.float16
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 intention_class = ['interacting', 'interested', 'not_interested']
 attitude_classes = ['positive', 'negative', 'no_interacting']
 action_classes = ['hand_shake', 'hug', 'pet', 'wave', 'punch', 'throw', 'point-converse', 'gaze', 'leave',
