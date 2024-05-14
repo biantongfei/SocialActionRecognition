4fd4e45026f838e76f17584f6a58bacfd7a982c3
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-14 20:01:47 +0100

    add confident score

--- play.py
+++ play.py
@@ -1,9 +1,6 @@
 import torch
 
-# 假设你的输入数据尺寸为 (30, 30, 26, 3)
-input_data = torch.randn(30, 30, 26, 3)
+a = torch.zeros((2, 3))
 
-# 使用 torch.permute() 函数调整输入数据的维度顺序
-input_data = input_data.permute(0, 3, 1, 2, 4)
-
-print("调整后的输入数据尺寸:", input_data.shape)
+a = a + torch.full((2, 3), 1)
+print(a)
