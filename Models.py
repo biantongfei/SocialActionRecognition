94c8fa082795af4d964437af34abc560e44c0e84
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-14 21:20:14 +0100

    add confident score

--- Models.py
+++ Models.py
@@ -22,7 +22,7 @@ elif torch.backends.mps.is_available():
     device = torch.device('mps')
 else:
     device = torch.device('cpu')
-dtype = torch.float16
+dtype = torch.float32
 
 
 class DNN(nn.Module):
@@ -289,7 +289,7 @@ class GNN(nn.Module):
         self.framework = framework
         self.model = model
         self.max_length = max_length
-        self.keypoint_hidden_dim = 128
+        self.keypoint_hidden_dim = 64
         self.pooling = False
         self.pooling_rate = 0.6 if self.pooling else 1
         if body_part[0]:
--- train_val.py
+++ train_val.py
@@ -75,7 +75,7 @@ elif torch.backends.mps.is_available():
 else:
     print('Using CPU for training')
     device = torch.device('cpu')
-dtype = torch.float16
+dtype = torch.float32
 intention_class = ['interacting', 'interested', 'not_interested']
 attitude_classes = ['positive', 'negative', 'no_interacting']
 action_classes = ['hand_shake', 'hug', 'pet', 'wave', 'punch', 'throw', 'point-converse', 'gaze', 'leave',
