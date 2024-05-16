1b06b4ae311935dba897beea484a0529c7911e29
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-16 23:49:32 +0100

    add confident score

--- constants.py
+++ constants.py
@@ -29,7 +29,7 @@ rnn_batch_size = 128
 conv1d_batch_size = 128
 gcn_batch_size = 128
 stgcn_batch_size = 64
-learning_rate = 1e-2
+learning_rate = 1e-3
 if torch.cuda.is_available():
     print('Using CUDA for training')
     device = torch.device("cuda:0")
