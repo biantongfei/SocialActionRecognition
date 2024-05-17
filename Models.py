1328b300e6af5c087a58a30d3f5a6f7d72f27a26
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-17 21:48:36 +0100

    add confident score

--- Models.py
+++ Models.py
@@ -319,8 +319,8 @@ class GNN(nn.Module):
             # self.GCN_hand = GAT(in_channels=3, hidden_channels=self.keypoint_hidden_dim, num_layers=3)
             # self.GCN_hand = GIN(in_channels=3, hidden_channels=self.keypoint_hidden_dim, num_layers=3)
         self.gcn_bn = nn.BatchNorm1d(int(self.keypoint_hidden_dim * self.input_size / 3))
-        # self.gcn_attention = nn.Linear(int(self.keypoint_hidden_dim * self.input_size / 3), 1)
-        self.gcn_attention = SelfAttention(int(self.keypoint_hidden_dim * self.input_size / 3))
+        self.gcn_attention = nn.Linear(int(self.keypoint_hidden_dim * self.input_size / 3), 1)
+        # self.gcn_attention = SelfAttention(int(self.keypoint_hidden_dim * self.input_size / 3))
         if self.model in ['gcn_lstm', 'gcn_gru']:
             self.time_model = nn.LSTM(math.ceil(self.pooling_rate * self.input_size / 3) * self.keypoint_hidden_dim,
                                       hidden_size=256, num_layers=3, bidirectional=True,
@@ -432,8 +432,8 @@ class GNN(nn.Module):
         # x = self.gcn_bn(x)
         # x, _ = self.gcn_attention(x)
         # x = x.view(-1, self.max_length, int(self.keypoint_hidden_dim * self.input_size / 3))
-        # attention_weights = nn.Softmax(dim=1)(self.gcn_attention(x))
-        # x = x * attention_weights
+        attention_weights = nn.Softmax(dim=1)(self.gcn_attention(x))
+        x = x * attention_weights
         if self.model in ['gcn_lstm', 'gcn_gru']:
             on, _ = self.time_model(x)
             on = on.view(on.shape[0], on.shape[1], 2, -1)
