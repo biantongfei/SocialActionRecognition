0cb6628feff083ec565d85535f4b52ed5eb8c46e
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-18 13:04:41 +0100

    add confident score

--- Models.py
+++ Models.py
@@ -16,28 +16,6 @@ attitude_class_num = len(attitude_classes)
 action_class_num = len(action_classes)
 
 
-class SelfAttention(nn.Module):
-    def __init__(self, embed_dim):
-        super(SelfAttention, self).__init__()
-        self.embed_dim = embed_dim
-        self.query = nn.Linear(embed_dim, embed_dim)
-        self.key = nn.Linear(embed_dim, embed_dim)
-        self.value = nn.Linear(embed_dim, embed_dim)
-
-    def forward(self, x):
-        q = self.query(x)
-        k = self.key(x)
-        v = self.value(x)
-        scores = torch.matmul(q, k.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.embed_dim).float())
-
-        # 使用 softmax 函数将分数转换为权重
-        weights = F.softmax(scores, dim=-1)
-
-        # 加权求和
-        weighted_sum = torch.matmul(weights, v)
-        return weighted_sum, weights
-
-
 class DNN(nn.Module):
     def __init__(self, is_coco, body_part, framework):
         super(DNN, self).__init__()
@@ -319,8 +297,7 @@ class GNN(nn.Module):
             # self.GCN_hand = GAT(in_channels=3, hidden_channels=self.keypoint_hidden_dim, num_layers=3)
             # self.GCN_hand = GIN(in_channels=3, hidden_channels=self.keypoint_hidden_dim, num_layers=3)
         self.gcn_bn = nn.BatchNorm1d(int(self.keypoint_hidden_dim * self.input_size / 3))
-        # self.gcn_attention = nn.Linear(int(self.keypoint_hidden_dim * self.input_size / 3), 1)
-        self.gcn_attention = SelfAttention(int(self.keypoint_hidden_dim * self.input_size / 3))
+        self.gcn_attention = nn.Linear(int(self.keypoint_hidden_dim * self.input_size / 3), 1)
         if self.model in ['gcn_lstm', 'gcn_gru']:
             self.time_model = nn.LSTM(math.ceil(self.pooling_rate * self.input_size / 3) * self.keypoint_hidden_dim,
                                       hidden_size=256, num_layers=3, bidirectional=True,
@@ -429,11 +406,10 @@ class GNN(nn.Module):
             x_list.append(x_hand)
         x = torch.cat(x_list, dim=2)
         x = x.view(-1, int(self.keypoint_hidden_dim * self.input_size / 3))
-        # x = self.gcn_bn(x)
-        x, _ = self.gcn_attention(x)
+        x = self.gcn_bn(x)
         x = x.view(-1, self.max_length, int(self.keypoint_hidden_dim * self.input_size / 3))
-        # attention_weights = nn.Softmax(dim=1)(self.gcn_attention(x))
-        # x = x * attention_weights
+        attention_weights = nn.Softmax(dim=1)(self.gcn_attention(x))
+        x = x * attention_weights
         if self.model in ['gcn_lstm', 'gcn_gru']:
             on, _ = self.time_model(x)
             on = on.view(on.shape[0], on.shape[1], 2, -1)
--- constants.py
+++ constants.py
@@ -30,6 +30,9 @@ conv1d_batch_size = 128
 gcn_batch_size = 128
 stgcn_batch_size = 64
 learning_rate = 1e-3
+# dropout_rate = 0.5
+dropout_rate = 0
+
 if torch.cuda.is_available():
     print('Using CUDA for training')
     device = torch.device("cuda:0")
