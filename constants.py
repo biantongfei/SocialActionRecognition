d0ec56e60451c3d5117191f85382acdb788f8eb7
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-24 21:28:51 +0100

    MS-G3D

--- constants.py
+++ constants.py
@@ -28,7 +28,7 @@ perframe_batch_size = 2048
 rnn_batch_size = 128
 conv1d_batch_size = 128
 gcn_batch_size = 128
-stgcn_batch_size = 64
+stgcn_batch_size = 32
 msgcn_batch_size = 16
 learning_rate = 1e-3
 
