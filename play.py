b523352e64150a82904244788f19acd379604085
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-25 02:12:33 +0100

    MS-G3D

--- play.py
+++ play.py
@@ -1,2 +1,8 @@
-a = 1
-print(type(a) == int)
+import torch
+from torch_geometric.utils import add_self_loops
+
+a = [[0, 1, 2, 3], [1, 2, 3, 4]]
+a = torch.Tensor(a)
+a = torch.cat([a, a.flip([0])], dim=1)
+a, _ = add_self_loops(a, num_nodes=4)
+print(a)
