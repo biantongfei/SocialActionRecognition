868da618dac420c12225e552ef80391cd08e9f84
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-25 18:34:25 +0100

    MS-G3D

--- train_val.py
+++ train_val.py
@@ -11,6 +11,7 @@ from torch.nn import functional
 import torch.nn.utils.rnn as rnn_utils
 from torch.optim.lr_scheduler import StepLR
 
+import numpy as np
 from sklearn.metrics import f1_score
 import csv
 import smtplib
@@ -44,6 +45,7 @@ def draw_save(name, performance_model, framework):
             if 'intention' in tasks:
                 data.append(p_m['intention_accuracy'])
                 data.append(p_m['intention_f1'])
+                data.append(p_m['intention_confidence_score'])
                 if index == 0:
                     int_y_true = p_m['intention_y_true']
                     int_y_pred = p_m['intention_y_pred']
@@ -53,6 +55,7 @@ def draw_save(name, performance_model, framework):
             if 'attitude' in tasks:
                 data.append(p_m['attitude_accuracy'])
                 data.append(p_m['attitude_f1'])
+                data.append(p_m['attitude_confidence_score'])
                 if index == 0:
                     att_y_true = p_m['attitude_y_true']
                     att_y_pred = p_m['attitude_y_pred']
@@ -62,6 +65,7 @@ def draw_save(name, performance_model, framework):
             if 'action' in tasks:
                 data.append(p_m['action_accuracy'])
                 data.append(p_m['action_f1'])
+                data.append(p_m['action_confidence_score'])
                 if index == 0:
                     act_y_true = p_m['action_y_true']
                     act_y_pred = p_m['action_y_pred']
@@ -101,7 +105,8 @@ def train(model, body_part, framework, frame_sample_hop, sequence_length=99999, 
     # dataset = 'noise+coco'
     tasks = [framework] if framework in ['intention', 'attitude', 'action'] else ['intention', 'attitude', 'action']
     for t in tasks:
-        performance_model = {'%s_accuracy' % t: None, '%s_f1' % t: None, '%s_y_true' % t: None, '%s_y_pred' % t: None}
+        performance_model = {'%s_accuracy' % t: None, '%s_f1' % t: None, '%s_confidence_score' % t: None,
+                             '%s_y_true' % t: None, '%s_y_pred' % t: None}
     num_workers = 8
     if model == 'avg':
         batch_size = avg_batch_size
@@ -197,7 +202,7 @@ def train(model, body_part, framework, frame_sample_hop, sequence_length=99999, 
         scheduler.step()
         progress_bar.close()
         print('Validating')
-        int_y_true, int_y_pred, att_y_true, att_y_pred, act_y_true, act_y_pred = [], [], [], [], [], []
+        int_y_true, int_y_pred, int_y_score, att_y_true, att_y_pred, att_y_score, act_y_true, act_y_pred, act_y_score = [], [], [], [], [], [], [], [], []
         net.eval()
         for data in val_loader:
             if model in ['avg', 'perframe', 'conv1d']:
@@ -220,17 +225,23 @@ def train(model, body_part, framework, frame_sample_hop, sequence_length=99999, 
             else:
                 int_outputs, att_outputs, act_outputs = net(inputs)
             if 'intention' in tasks:
-                int_pred = int_outputs.argmax(dim=1)
+                score, pred = torch.max(int_outputs, dim=1)
+                # int_pred = int_outputs.argmax(dim=1)
                 int_y_true += int_labels.tolist()
-                int_y_pred += int_pred.tolist()
+                int_y_pred += pred.tolist()
+                int_y_score += score.tolist()
             if 'attitude' in tasks:
-                att_pred = att_outputs.argmax(dim=1)
+                score, pred = torch.max(att_outputs, dim=1)
+                # att_pred = att_outputs.argmax(dim=1)
                 att_y_true += att_labels.tolist()
-                att_y_pred += att_pred.tolist()
+                att_y_pred += pred.tolist()
+                att_y_score += score.tolist()
             if 'action' in tasks:
-                act_pred = act_outputs.argmax(dim=1)
+                score, pred = torch.max(act_outputs, dim=1)
+                # act_pred = act_outputs.argmax(dim=1)
                 act_y_true += act_labels.tolist()
-                act_y_pred += act_pred.tolist()
+                act_y_pred += pred.tolist()
+                act_y_score += score.tolist()
         result_str = 'model: %s, epoch: %d, ' % (model, epoch)
         if 'intention' in tasks:
             int_y_true, int_y_pred = torch.Tensor(int_y_true), torch.Tensor(int_y_pred)
@@ -238,21 +249,27 @@ def train(model, body_part, framework, frame_sample_hop, sequence_length=99999, 
                 int_y_true, int_y_pred = transform_preframe_result(int_y_true, int_y_pred, sequence_length)
             int_acc = int_y_pred.eq(int_y_true).sum().float().item() / int_y_pred.size(dim=0)
             int_f1 = f1_score(int_y_true, int_y_pred, average='weighted')
-            result_str += 'int_acc: %.2f, int_f1: %.4f, ' % (int_acc * 100, int_f1)
+            int_score = np.mean(int_y_score)
+            result_str += 'int_acc: %.2f, int_f1: %.4f, int_confidence_score: %.4f, ' % (
+                int_acc * 100, int_f1, int_score)
         if 'attitude' in tasks:
             att_y_true, att_y_pred = torch.Tensor(att_y_true), torch.Tensor(att_y_pred)
             if model == 'perframe':
                 att_y_true, att_y_pred = transform_preframe_result(att_y_true, att_y_pred, sequence_length)
             att_acc = att_y_pred.eq(att_y_true).sum().float().item() / att_y_pred.size(dim=0)
             att_f1 = f1_score(att_y_true, att_y_pred, average='weighted')
-            result_str += 'att_acc: %.2f, att_f1: %.4f, ' % (att_acc * 100, att_f1)
+            att_score = np.mean(att_y_score)
+            result_str += 'att_acc: %.2f, att_f1: %.4f, att_confidence_score: %.4f, ' % (
+                att_acc * 100, att_f1, att_score)
         if 'action' in tasks:
             act_y_true, act_y_pred = torch.Tensor(act_y_true), torch.Tensor(act_y_pred)
             if model == 'perframe':
                 act_y_true, act_y_pred = transform_preframe_result(act_y_true, act_y_pred, sequence_length)
             act_acc = act_y_pred.eq(act_y_true).sum().float().item() / act_y_pred.size(dim=0)
             act_f1 = f1_score(act_y_true, act_y_pred, average='weighted')
-            result_str += 'act_acc: %.2f%%, act_f1: %.4f, ' % (act_acc * 100, act_f1)
+            act_score = np.mean(act_y_score)
+            result_str += 'act_acc: %.2f%%, act_f1: %.4f, act_confidence_score: %.4f, ' % (
+                act_acc * 100, act_f1, act_score)
             print(result_str + 'loss: %.4f' % total_loss)
             torch.cuda.empty_cache()
         # if int_f1 <= intention_best_f1 and att_f1 <= attitude_best_f1 and act_f1 <= action_best_f1:
@@ -269,7 +286,7 @@ def train(model, body_part, framework, frame_sample_hop, sequence_length=99999, 
     print('Testing')
     test_loader = JPLDataLoader(is_coco=is_coco, model=model, dataset=testset, sequence_length=sequence_length,
                                 batch_size=batch_size, drop_last=False, num_workers=num_workers)
-    int_y_true, int_y_pred, att_y_true, att_y_pred, act_y_true, act_y_pred = [], [], [], [], [], []
+    int_y_true, int_y_pred, int_y_score, att_y_true, att_y_pred, att_y_score, act_y_true, act_y_pred, act_y_score = [], [], [], [], [], [], [], [], []
     process_time = 0
     start_time = time.time()
     net.eval()
@@ -297,17 +314,23 @@ def train(model, body_part, framework, frame_sample_hop, sequence_length=99999, 
         else:
             int_outputs, att_outputs, act_outputs = net(inputs)
         if 'intention' in tasks:
-            int_pred = int_outputs.argmax(dim=1)
+            score, pred = torch.max(int_outputs, dim=1)
+            # int_pred = int_outputs.argmax(dim=1)
             int_y_true += int_labels.tolist()
-            int_y_pred += int_pred.tolist()
+            int_y_pred += pred.tolist()
+            int_y_score += score.tolist()
         if 'attitude' in tasks:
-            att_pred = att_outputs.argmax(dim=1)
+            score, pred = torch.max(att_outputs, dim=1)
+            # att_pred = att_outputs.argmax(dim=1)
             att_y_true += att_labels.tolist()
-            att_y_pred += att_pred.tolist()
+            att_y_pred += pred.tolist()
+            att_y_score += score.tolist()
         if 'action' in tasks:
-            act_pred = act_outputs.argmax(dim=1)
+            score, pred = torch.max(act_outputs, dim=1)
+            # act_pred = act_outputs.argmax(dim=1)
             act_y_true += act_labels.tolist()
-            act_y_pred += act_pred.tolist()
+            act_y_pred += pred.tolist()
+            act_y_score += score.tolist()
         torch.cuda.empty_cache()
     end_time = time.time()
     process_time += end_time - start_time
@@ -318,33 +341,39 @@ def train(model, body_part, framework, frame_sample_hop, sequence_length=99999, 
             int_y_true, int_y_pred = transform_preframe_result(int_y_true, int_y_pred, sequence_length)
         int_acc = int_y_pred.eq(int_y_true).sum().float().item() / int_y_pred.size(dim=0)
         int_f1 = f1_score(int_y_true, int_y_pred, average='weighted')
+        int_score = np.mean(int_y_score)
         performance_model['intention_accuracy'] = int_acc
         performance_model['intention_f1'] = int_f1
+        performance_model['intention_confidence_score'] = int_score
         performance_model['intention_y_true'] = int_y_true
         performance_model['intention_y_pred'] = int_y_pred
-        result_str += 'int_acc: %.2f, int_f1: %.4f, ' % (int_acc * 100, int_f1)
+        result_str += 'int_acc: %.2f, int_f1: %.4f, int_confidence_score :%.4f, ' % (int_acc * 100, int_f1, int_score)
     if 'attitude' in tasks:
         att_y_true, att_y_pred = torch.Tensor(att_y_true), torch.Tensor(att_y_pred)
         if model == 'perframe':
             att_y_true, att_y_pred = transform_preframe_result(att_y_true, att_y_pred, sequence_length)
         att_acc = att_y_pred.eq(att_y_true).sum().float().item() / att_y_pred.size(dim=0)
         att_f1 = f1_score(att_y_true, att_y_pred, average='weighted')
+        att_score = np.mean(att_y_score)
         performance_model['attitude_accuracy'] = att_acc
         performance_model['attitude_f1'] = att_f1
+        performance_model['attitude_confidence_score'] = att_score
         performance_model['attitude_y_true'] = att_y_true
         performance_model['attitude_y_pred'] = att_y_pred
-        result_str += 'att_acc: %.2f, att_f1: %.4f, ' % (att_acc * 100, att_f1)
+        result_str += 'att_acc: %.2f, att_f1: %.4f, att_confidence_score: %.4f, ' % (att_acc * 100, att_f1, att_score)
     if 'action' in tasks:
         act_y_true, act_y_pred = torch.Tensor(act_y_true), torch.Tensor(act_y_pred)
         if model == 'perframe':
             act_y_true, act_y_pred = transform_preframe_result(act_y_true, act_y_pred, sequence_length)
         act_acc = act_y_pred.eq(act_y_true).sum().float().item() / act_y_pred.size(dim=0)
         act_f1 = f1_score(act_y_true, act_y_pred, average='weighted')
+        act_score = np.mean(act_y_score)
         performance_model['action_accuracy'] = act_acc
         performance_model['action_f1'] = act_f1
+        performance_model['action_confidence_score'] = act_score
         performance_model['action_y_true'] = act_y_true
         performance_model['action_y_pred'] = act_y_pred
-        result_str += 'act_acc: %.2f, act_f1: %.4f, ' % (act_acc * 100, act_f1)
+        result_str += 'act_acc: %.2f, act_f1: %.4f, act_confidence_score: %.4f, ' % (act_acc * 100, act_f1, act_score)
     print(result_str + 'Model Size: %.2f MB, process_time_pre_frame: %.3f ms' % (
         (MFlops, process_time * 1000 / len(testset))))
     print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
@@ -353,8 +382,8 @@ def train(model, body_part, framework, frame_sample_hop, sequence_length=99999, 
 
 
 if __name__ == '__main__':
-    # model = 'avg'
-    model = 'conv1d'
+    model = 'avg'
+    # model = 'conv1d'
     # model = 'lstm'
     # model = 'gcn_conv1d'
     # model = 'gcn_lstm'
