6e4d3a319e5048cfb9c11dfdd6631d0dee4da3b6
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-27 12:30:01 +0100

    MS-G3D

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ model = 'gcn_lstm'
 framework = 'parallel'
 # framework = 'tree'
 # framework = 'chain'
-ori_video = True
+ori_video = False
 frame_sample_hop = 1
 sequence_length = 30
 for framework in ['parallel']:
