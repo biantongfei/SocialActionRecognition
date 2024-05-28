10701b0a2cd3690f75843076dac9a150af163b82
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-28 19:44:59 +0100

    MS-G3D

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ for sequence_length in [20, 25, 35, 40]:
         #     continue
         performance_model.append(p_m)
         i += 1
-    draw_save(str(body_part), performance_model, framework)
+    draw_save(sequence_length, performance_model, framework)
     result_str = 'model: %s, body_part: [%s, %s, %s], framework: %s, sequence_length: %d, frame_hop: %s' % (
         model, body_part[0], body_part[1], body_part[2], framework, sequence_length, frame_sample_hop)
     print(result_str)
