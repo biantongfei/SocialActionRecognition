5f3f94cadfd72d109524b3b2907abfbd0d397867
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-28 11:33:41 +0100

    MS-G3D

--- play.py
+++ play.py
@@ -1,8 +1,2 @@
-import torch
-from torch_geometric.utils import add_self_loops
-
-a = [[0, 1, 2, 3], [1, 2, 3, 4]]
-a = torch.Tensor(a)
-a = torch.cat([a, a.flip([0])], dim=1)
-a, _ = add_self_loops(a, num_nodes=4)
-print(a)
+a = [True, True]
+print(str(a))
--- train.py
+++ train.py
@@ -13,7 +13,7 @@ ori_video = False
 frame_sample_hop = 1
 sequence_length = 30
 # for framework in ['parallel','tree','chain']:
-for sequence_length in [20, 25, 35, 40]:
+for body_part in [[True, False, False], [False, True, False], [False, False, True]]:
     performance_model = []
     i = 0
     while i < 10:
@@ -29,7 +29,7 @@ for sequence_length in [20, 25, 35, 40]:
         #     continue
         performance_model.append(p_m)
         i += 1
-    draw_save(str(sequence_length), performance_model, framework)
+    draw_save(str(body_part), performance_model, framework)
     result_str = 'model: %s, body_part: [%s, %s, %s], framework: %s, sequence_length: %d, frame_hop: %s' % (
         model, body_part[0], body_part[1], body_part[2], framework, sequence_length, frame_sample_hop)
     print(result_str)
