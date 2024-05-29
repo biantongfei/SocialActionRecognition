2383757b5a16c98661c74dfada884f7d0205a4e4
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-05-29 22:22:50 +0100

    MS-G3D

--- train_val.py
+++ train_val.py
@@ -406,12 +406,12 @@ def train(model, body_part, framework, frame_sample_hop, sequence_length=99999, 
 
 if __name__ == '__main__':
     # model = 'avg'
-    # model = 'conv1d'
+    model = 'conv1d'
     # model = 'lstm'
     # model = 'gcn_conv1d'
     # model = 'gcn_lstm'
     # model = 'gcn_gcn'
-    model = 'stgcn'
+    # model = 'stgcn'
     # model = 'msgcn'
     body_part = [True, True, True]
 
