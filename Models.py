4a734e077873d6b7ba3dcab116656f70272b0b3c
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-06-10 17:09:05 +0100

    Transformer

--- Models.py
+++ Models.py
@@ -383,9 +383,7 @@ class GNN(nn.Module):
             self.GCN_hand = GCN(in_channels=3, hidden_channels=self.keypoint_hidden_dim, num_layers=3)
             # self.GCN_hand = GAT(in_channels=3, hidden_channels=self.keypoint_hidden_dim, num_layers=3)
             # self.GCN_hand = GIN(in_channels=3, hidden_channels=self.keypoint_hidden_dim, num_layers=3)
-        # self.gcn_attention = nn.Linear(int(self.keypoint_hidden_dim * self.input_size / 3), 1)
-        self.gcn_attention = nn.MultiheadAttention(embed_dim=int(self.keypoint_hidden_dim * self.input_size / 3),
-                                                   num_heads=8)
+        self.gcn_attention = nn.Linear(sequence_length, 1)
         if self.model == 'gcn_lstm':
             # self.time_model = nn.LSTM(math.ceil(self.pooling_rate * self.input_size / 3) * self.keypoint_hidden_dim,
             #                           hidden_size=128, num_layers=3, bidirectional=True, batch_first=True)
@@ -516,13 +514,12 @@ class GNN(nn.Module):
             x_hand = x_hand.view(-1, self.sequence_length, self.keypoint_hidden_dim * hands_point_num)
             x_list.append(x_hand)
         x = torch.cat(x_list, dim=2)
-        x = x.permute(1, 0, 2)
+        x = x.permute(0, 2, 1)
+        gcn_attention_weights = nn.Softmax(dim=1)(self.gcn_attention(x))
+        x = x * gcn_attention_weights
         print(x.shape)
-        x, gcn_attention_weights = self.gcn_attention(x, x, x)
         print(gcn_attention_weights.shape)
-        x = x.permute(1, 0, 2)
-        # gcn_attention_weights = nn.Softmax(dim=1)(self.gcn_attention(x))
-        # x = x * gcn_attention_weights
+        x = x.permute(0, 2, 1)
         if self.model == 'gcn_lstm':
             on, _ = self.time_model(x)
             on = on.view(on.shape[0], on.shape[1], 2, -1)
