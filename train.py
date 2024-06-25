7a4d93372b15799bcf03170c95b22086315624d7
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-06-25 13:54:37 +0100

    Transformer

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ framework = 'chain'
 ori_video = False
 frame_sample_hop = 1
 sequence_length = 30
-dataset = 'mixed+coco'
+dataset = 'gen+coco'
 oneshot = True
 for model in ['gcn_lstm']:
     # for dataset in ['0+coco', '1+coco', '2+coco', '3+coco', '4+coco', '5+coco', '6+coco', '7+coco', '8+coco', '9+coco']:
