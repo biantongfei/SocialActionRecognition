b4db14f8fb911fcd3adbaa8e9d6694fb7469228c
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-07-01 19:23:56 +0100

    Transformer

--- Models.py
+++ Models.py
@@ -464,7 +464,7 @@ class GNN(nn.Module):
         self.intention_head = nn.Sequential(nn.ReLU(),
                                             nn.Linear(16, intention_class_num)
                                             )
-        self.other_parameters += self.intention_head[1].parameters()
+        self.other_parameters += self.intention_head.parameters()
         if self.framework in ['parallel', 'intention', 'attitude', 'action']:
             self.attitude_head = nn.Sequential(nn.ReLU(),
                                                nn.Linear(16, attitude_class_num)
@@ -472,7 +472,8 @@ class GNN(nn.Module):
             self.action_head = nn.Sequential(nn.ReLU(),
                                              nn.Linear(16, action_class_num)
                                              )
-            self.other_parameters += self.attitude_head[1].parameters() + self.action_head[1].parameters()
+            self.other_parameters += self.attitude_head.parameters()
+            self.other_parameters += self.action_head.parameters()
         elif self.framework == 'tree':
             self.attitude_head = nn.Sequential(nn.BatchNorm1d(16 + intention_class_num),
                                                nn.ReLU(),
@@ -482,7 +483,8 @@ class GNN(nn.Module):
                                              nn.ReLU(),
                                              nn.Linear(16 + intention_class_num, action_class_num)
                                              )
-            self.other_parameters += self.attitude_head[2].parameters() + self.action_head[2].parameters()
+            self.other_parameters += self.attitude_head.parameters()
+            self.other_parameters += self.action_head.parameters()
         elif self.framework == 'chain':
             self.attitude_head = nn.Sequential(nn.BatchNorm1d(16 + intention_class_num),
                                                nn.ReLU(),
@@ -492,7 +494,8 @@ class GNN(nn.Module):
                                              nn.ReLU(),
                                              nn.Linear(16 + intention_class_num + attitude_class_num, action_class_num)
                                              )
-            self.other_parameters += self.attitude_head[2].parameters() + self.action_head[2].parameters()
+            self.other_parameters += self.attitude_head.parameters()
+            self.other_parameters += self.action_head.parameters()
 
     def forward(self, data):
         x_list = []
