128368e360602b9b91b3ac0b95799ac14ae51af2
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-07-12 17:45:06 +0100

    R3D

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ model = 'gcn_lstm'
 # framework = 'intention'
 # framework = 'attitude'
 # framework = 'action'
-framework = 'chain'
+# framework = 'parallel'
 # framework = 'tree'
 # framework = 'chain'
 ori_video = False
@@ -15,11 +15,11 @@ frame_sample_hop = 1
 sequence_length = 30
 dataset = 'mixed+coco'
 oneshot = False
-for model in ['gcn_lstm']:
-# for body_part in [[False, True, False], [False, False, True], [False, True, True]]:
+for framework in ['intention', 'attitude', 'action']:
+    # for body_part in [[False, True, False], [False, False, True], [False, True, True]]:
     # for framework in ['parallel', 'tree', 'chain']:
     # for model in ['msgcn', 'dgstgcn']:
-# for sequence_length in [5,10,15,20,25,35,40]:
+    # for sequence_length in [5,10,15,20,25,35,40]:
     performance_model = []
     i = 0
     while i < 5:
@@ -35,7 +35,7 @@ for model in ['gcn_lstm']:
         #     continue
         performance_model.append(p_m)
         i += 1
-    draw_save(model, performance_model, framework)
+    draw_save(framework, performance_model, framework)
     result_str = 'model: %s, body_part: [%s, %s, %s], framework: %s, sequence_length: %d, frame_hop: %s' % (
         model, body_part[0], body_part[1], body_part[2], framework, sequence_length, frame_sample_hop)
     print(result_str)
