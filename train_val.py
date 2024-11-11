cc15476b7ee3031f66b26c3b572ca266a9e0111d
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-11-11 22:16:18 +0000

    harper

--- train_val.py
+++ train_val.py
@@ -756,7 +756,7 @@ def train_harper(model, sequence_length, body_part, pretrained=True, new_classif
 
         print(result_str + 'loss: %.4f' % total_loss)
         torch.cuda.empty_cache()
-        if epoch == 10:
+        if epoch == 20:
             break
         else:
             epoch += 1
