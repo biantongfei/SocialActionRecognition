665c88ae5a30f3afd4545c0545d638f5304f79f2
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-11-11 21:01:42 +0000

    harper

--- Models.py
+++ Models.py
@@ -93,6 +93,7 @@ class Classifier(nn.Module):
                 y3 = self.action_head(torch.cat((y, y1, y2), dim=1))
             return y1, y2, y3
         elif self.framework == 'chain+contact':
+            print(y.shape)
             y1 = self.intention_head(y)
             y2 = self.attitude_head(torch.cat((y, y1), dim=1))
             y3 = self.action_head(torch.cat((y, y1, y2), dim=1))
