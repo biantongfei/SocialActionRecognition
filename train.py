9883611379905c9e87b90a6cd3f13b7e49fbe9dc
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-11-20 23:06:05 +0000

    loss

--- train.py
+++ train.py
@@ -39,10 +39,9 @@ if __name__ == '__main__':
             'goal': 'maximize',
         },
         'parameters': {
-            # 'epochs':{"values":[40,50]},
-            # 'times': {'values': [i for i in range(10)]},
-            # 'fc_hidden2': {'values': [8, 16, 32]},
-            'fc_hidden2': {'values': [8]},
+            'epochs':{"values":[40,50]},
+            'times': {'values': [i for i in range(10)]},
+            'fc_hidden2': {'values': [8, 16, 32]},
             'loss_type': {'values': ['pareto', 'dwa', 'sum', 'dynamic', 'uncertain']},
         }
     }
