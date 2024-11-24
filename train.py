b507ff70224463b18299184488b1533cb90d4ba3
Tongfei Bian <70830372+biantongfei@users.noreply.github.com>
2024-11-24 02:51:11 +0000

    harper

--- train.py
+++ train.py
@@ -20,55 +20,55 @@ sequence_length = 10
 
 
 # HARPER Dataset
-trainset, valset, testset = get_harper_dataset(body_part, sequence_length)
+# trainset, valset, testset = get_harper_dataset(body_part, sequence_length)
 
 
 def train():
-    # body_part = wandb.config.body_part
-    # p_m = train_jpl(wandb=wandb, model=model, body_part=body_part, framework=framework, sequence_length=sequence_length,
-    #                 frame_sample_hop=frame_sample_hop, trainset=trainset, valset=valset, testset=testset)
-    p_m = train_harper(wandb=wandb, model=model, sequence_length=sequence_length, trainset=trainset, valset=valset,
-                       testset=testset)
+    body_part = wandb.config.body_part
+    p_m = train_jpl(wandb=wandb, model=model, body_part=body_part, framework=framework, sequence_length=sequence_length,
+                    frame_sample_hop=frame_sample_hop, trainset=trainset, valset=valset, testset=testset)
+    # p_m = train_harper(wandb=wandb, model=model, sequence_length=sequence_length, trainset=trainset, valset=valset,
+    #                    testset=testset)
     # draw_save(framework, performance_model, framework)
     # send_email(result_str)
 
 
-sweep_config = {
-    'method': 'grid',
-    'metric': {
-        'name': 'avg_f1',
-        'goal': 'maximize',
-    },
-    'parameters': {
-        'epochs': {"values": [30, 35, 40, 45, 50, 55, 60]},
-        'loss_type': {'values': ['sum']},
-        'pretrained': {'values': [False]},
-        'new_classifier': {'values': [False]},
-        'times': {'values': [ii for ii in range(10)]}
-    }
-}
-sweep_id = wandb.sweep(sweep_config,
-                       project='SocialEgoNet_HARPER_fps%d' % int(sequence_length / frame_sample_hop))
-wandb.agent(sweep_id, function=train)
-#
-# body_part_list = [[True, False, False], [False, True, False], [False, False, True], [True, True, False],
-#                   [True, False, True], [False, True, True], [True, True, True]]
-# for body_part in body_part_list:
-#     trainset, valset, testset = get_jpl_dataset(model, body_part, frame_sample_hop, sequence_length,
-#                                                 augment_method='mixed', ori_videos=ori_video)
-#     sweep_config = {
-#         'method': 'grid',
-#         'metric': {
-#             'name': 'avg_f1',
-#             'goal': 'maximize',
-#         },
-#         'parameters': {
-#             'epochs': {"values": [40]},
-#             'body_part': {'values': [body_part]},
-#             'times': {'values': [ii for ii in range(10)]}
-#         }
+# sweep_config = {
+#     'method': 'grid',
+#     'metric': {
+#         'name': 'avg_f1',
+#         'goal': 'maximize',
+#     },
+#     'parameters': {
+#         'epochs': {"values": [30, 35, 40, 45, 50, 55, 60]},
+#         'loss_type': {'values': ['sum']},
+#         'pretrained': {'values': [False]},
+#         'new_classifier': {'values': [False]},
+#         'times': {'values': [ii for ii in range(10)]}
 #     }
-#     # wandb.init(project='SocialEgoNet', name='%s_%s' % (name, datetime.now().strftime("%Y-%m-%d_%H:%M")), config=config)
-#     sweep_id = wandb.sweep(sweep_config,
-#                            project='SocialEgoNet_JPL_fps%d_body_part' % int(sequence_length / frame_sample_hop))
-#     wandb.agent(sweep_id, function=train)
+# }
+# sweep_id = wandb.sweep(sweep_config,
+#                        project='SocialEgoNet_HARPER_fps%d' % int(sequence_length / frame_sample_hop))
+# wandb.agent(sweep_id, function=train)
+#
+body_part_list = [[True, False, False], [False, True, False], [False, False, True], [True, True, False],
+                  [True, False, True], [False, True, True], [True, True, True]]
+for body_part in body_part_list:
+    trainset, valset, testset = get_jpl_dataset(model, body_part, frame_sample_hop, sequence_length,
+                                                augment_method='mixed', ori_videos=ori_video)
+    sweep_config = {
+        'method': 'grid',
+        'metric': {
+            'name': 'avg_f1',
+            'goal': 'maximize',
+        },
+        'parameters': {
+            'epochs': {"values": [40]},
+            'body_part': {'values': [body_part]},
+            'times': {'values': [ii for ii in range(10)]}
+        }
+    }
+    # wandb.init(project='SocialEgoNet', name='%s_%s' % (name, datetime.now().strftime("%Y-%m-%d_%H:%M")), config=config)
+    sweep_id = wandb.sweep(sweep_config,
+                           project='SocialEgoNet_JPL_fps%d_body_part' % int(sequence_length / frame_sample_hop))
+    wandb.agent(sweep_id, function=train)
