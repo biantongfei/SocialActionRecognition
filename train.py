ed59513d3932739d63b3cf19677822393ece499e
biantongfei <70830372+biantongfei@users.noreply.github.com>
2024-11-30 14:10:48 +0000

    msg3d

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ sweep_config = {
         'goal': 'maximize',
     },
     'parameters': {
-        'epochs': {"values": [30, 40, 50]},
+        'epochs': {"values": [20, 30, 40]},
         'loss_type': {"values": ['pareto', 'dynamic', 'uncertain']},
         'times': {'values': [ii for ii in range(6)]},
         'keypoints_hidden_dim': {"values": [16]},
