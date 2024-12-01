9c7f62ba516c2bd90099eec0965ec53bce13c90a
biantongfei <70830372+biantongfei@users.noreply.github.com>
2024-12-01 23:29:20 +0000

    teacher-student

--- Dataset.py
+++ Dataset.py
@@ -26,6 +26,7 @@ def get_tra_test_files():
     tra_files = [i for i in os.listdir('../JPL_Augmented_Posefeatures/mixed/coco_wholebody/train/') if 'json' in i]
     val_files = [i for i in os.listdir('../JPL_Augmented_Posefeatures/mixed/coco_wholebody/validation/') if 'json' in i]
     test_files = [i for i in os.listdir('../JPL_Augmented_Posefeatures/mixed/coco_wholebody/test/') if 'json' in i]
+    tra_files = [i for i in tra_files if 'ori_' in i]
     tra_files.sort()
     val_files.sort()
     test_files.sort()
