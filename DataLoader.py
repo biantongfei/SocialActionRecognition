6604aec96a5037e51b519949639a0371ac99767c
biantongfei <70830372+biantongfei@users.noreply.github.com>
2024-12-01 23:27:24 +0000

    teacher-student

--- DataLoader.py
+++ DataLoader.py
@@ -22,10 +22,10 @@ def rnn_collate_fn(data):
 
 class Pose_DataLoader(DataLoader):
     def __init__(self, model, dataset, batch_size, sequence_length, frame_sample_hop, drop_last=True, shuffle=False,
-                 num_workers=1, contact=False, seed_worker=None, generator=None):
+                 num_workers=1, contact=False, worker_init_fn=None, generator=None):
         super(Pose_DataLoader, self).__init__(dataset=dataset, batch_size=batch_size, shuffle=shuffle,
-                                              drop_last=drop_last, num_workers=num_workers, worker_init_fn=seed_worker,
-                                              generator=generator)
+                                              drop_last=drop_last, num_workers=num_workers,
+                                              worker_init_fn=worker_init_fn, generator=generator)
         if model in ['lstm', 'gru']:
             self.collate_fn = rnn_collate_fn
         elif model in ['conv1d', 'tran']:
