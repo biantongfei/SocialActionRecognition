2dd0335fb2a449937c47d7b48dcbba3c34f21418
biantongfei <70830372+biantongfei@users.noreply.github.com>
2024-12-03 21:48:38 +0000

    teacher-student

--- teacher_student.py
+++ teacher_student.py
@@ -30,9 +30,9 @@ def get_teacher_logist(teacher_model, dataset, batch_size, sequence_length, fram
         teacher_net.eval()
     teacher_dataloader = Pose_DataLoader('msgcn', dataset, batch_size, sequence_length, frame_sample_hop,
                                          drop_last=False)
-    teacher_logist = (torch.zeros(len(teacher_dataloader), len(intention_classes)),
-                      torch.zeros(len(teacher_dataloader), len(attitude_classes)),
-                      torch.zeros(len(teacher_dataloader), len(action_classes)))
+    teacher_logist = (torch.zeros(len(dataset), len(intention_classes)),
+                      torch.zeros(len(dataset), len(attitude_classes)),
+                      torch.zeros(len(dataset), len(action_classes)))
     print('Loading teacher logist')
     progress_bar = tqdm(total=len(teacher_dataloader), desc='Progress')
     for index, data in enumerate(teacher_dataloader):
