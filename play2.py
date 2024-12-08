fc8cf341dc113e48385a8ede528d522445e9d408
biantongfei <70830372+biantongfei@users.noreply.github.com>
2024-12-08 01:09:58 +0000

    teacher-student

--- play2.py
+++ play2.py
@@ -1,8 +1,1 @@
-import numpy as np
-
-features = np.array([1.2, 3.4, 5.6, 7.8])  # 特征序列
-binary_sequence = np.array([1, 0, 1, 0])    # 二元序列
-
-# 将二元序列为 0 的位置的特征置为 0
-filtered_features = features * binary_sequence
-print(filtered_features)
+print(2.0%1==0)
